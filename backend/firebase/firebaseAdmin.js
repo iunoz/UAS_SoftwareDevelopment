7cfb5dce5e0d2d949725e01afa5a76925b4c331f
Ryan Hermanto <ryanhermanto123z@gmail.com>
2025-06-17 02:00:39 +0700

    percobaan hosting

--- backend/firebase/firebaseAdmin.js
+++ backend/firebase/firebaseAdmin.js
@@ -1,30 +1,11 @@
 import admin from 'firebase-admin';
-
-let serviceAccount = {};
-try {
-  if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-    if (serviceAccount.private_key) {
-      serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
-    }
-    console.log('Firebase service account loaded:', serviceAccount.project_id ? 'project_id found' : 'project_id missing');
-    console.log('Parsed serviceAccount object:', serviceAccount);
-  } else {
-    console.warn('FIREBASE_SERVICE_ACCOUNT environment variable is not set.');
-  }
-} catch (error) {
-  console.error('Failed to parse FIREBASE_SERVICE_ACCOUNT environment variable:', error);
-}
+import serviceAccount from './serviceAccountKey.json' assert { type: 'json' };
 
 // Inisialisasi Firebase Admin SDK
 if (!admin.apps.length) {
-  if (serviceAccount && serviceAccount.project_id) {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    });
-  } else {
-    console.error('Invalid Firebase service account credentials. Initialization skipped.');
-  }
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
 }
 
 export default admin;
